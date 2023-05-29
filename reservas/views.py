7c4a1353691c8f224f80ee8057760b3fcce18d61
ihuntersouls <neiderzamoramoreno@gmail.com>
2023-05-28 19:50:52 -0500

    Response add

--- reservas/views.py
+++ reservas/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .serializer import ReservasSerializer
 from .models import Reservas
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
